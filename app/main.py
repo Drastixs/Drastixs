0babc7360960f563b8160028af50a27a7ef52db2
Jack Marshall <jack.er.marshall@gmail.com>
2025-10-21 14:54:21 +0100

    add exit built in command

--- app/main.py
+++ app/main.py
@@ -5,17 +5,20 @@ def main():
     while True:
         sys.stdout.write("$ ")
         user = input()
-        print(f"{user}: command not found")
         userSplit = user.split(" ")
         cmd = userSplit[0]
         args = []
         if len(userSplit) > 1:
             args = userSplit[1:]
+
+        #start of command check
         if cmd == "exit":
             if (len(args) != 1):
                 continue
             if args[0].isdigit():
                 return args[0] 
+        else:#command not found
+            print(f"{user}: command not found")
 
 if __name__ == "__main__":
     main()
